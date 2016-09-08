21adb7db16369ade54662ed4a8baefc02fdb290d
nerd100 <den.meyer1@web.de>
2016-09-08 23:02:06 +0200

    first commit

---
+++ app/src/main/java/com/example/dennis/nerdquiz/DatabaseHelper.java
@@ -0,0 +1,120 @@
+package com.example.dennis.nerdquiz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by Dennis on 25.08.2016.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+    String DB_PATH = null;
+    private static String DB_NAME = "Quiz.db";
+    private SQLiteDatabase myDatabase;
+    private Context context;
+    public static  final String DATABASE_NAME = "Quiz.db";
+    public static  final String TABLE_NAME = "quiz_table";
+    public static  final String COL_1 = "ID";
+    public static  final String COL_2 = "Category";
+    public static  final String COL_3 = "Difficulty";
+    public static  final String COL_4 = "Question";
+    public static  final String COL_5 = "RightAnswer";
+    public static  final String COL_6 = "FalseAnswer1";
+    public static  final String COL_7 = "FalseAnswer2";
+    public static  final String COL_8 = "FalseAnswer3";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        this.context = context;
+        this.DB_PATH = "/data/data/" + context.getPackageName() + "/" + "databases/";
+    }
+
+
+
+    public SQLiteDatabase openDatabase() {
+        File dbFile = context.getDatabasePath(DB_NAME);
+
+        if (!dbFile.exists()) {
+            Toast.makeText(context.getApplicationContext(), "No Database", Toast.LENGTH_SHORT).show();
+            try {
+                SQLiteDatabase checkDB = context.openOrCreateDatabase(DB_NAME, context.MODE_PRIVATE, null);
+                if(checkDB != null){
+
+                    checkDB.close();
+
+                }
+                copyDatabase(dbFile);
+            } catch (IOException e) {
+                throw new RuntimeException("Error creating source database", e);
+            }
+        }
+        Toast.makeText(context.getApplicationContext(), "Database", Toast.LENGTH_SHORT).show();
+        return SQLiteDatabase.openDatabase(dbFile.getPath(), null, SQLiteDatabase.OPEN_READONLY);
+    }
+
+    private void copyDatabase(File dbFile) throws IOException {
+        InputStream is = context.getAssets().open(DB_NAME);
+        OutputStream os = new FileOutputStream(dbFile);
+
+        byte[] buffer = new byte[1024];
+        while (is.read(buffer) > 0) {
+            os.write(buffer);
+        }
+
+        os.flush();
+        os.close();
+        is.close();
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "Category TEXT, Difficulty TEXT, Question TEXT, RightAnswer TEXT, FalseAnswer1 TEXT," +
+                " FalseAnswer2 TEXT, FalseAnswer3 TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String category, String difficulty, String question, String RA, String FA1, String FA2, String FA3){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, category);
+        contentValues.put(COL_3, difficulty);
+        contentValues.put(COL_4, question);
+        contentValues.put(COL_5, RA);
+        contentValues.put(COL_6, FA1);
+        contentValues.put(COL_7, FA2);
+        contentValues.put(COL_8, FA3);
+        long result = db.insert(TABLE_NAME,null,contentValues); //insert returns -1 if fails
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData(String category, String diff){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT Question,RightAnswer,FalseAnswer1,FalseAnswer2,FalseAnswer3" +
+                " from "+ TABLE_NAME+" WHERE (Category =  \""+category+"\") AND (Difficulty=\""+diff+"\")",null);
+        return res;
+    }
+    public Cursor getAllData(String diff){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT Question,RightAnswer,FalseAnswer1,FalseAnswer2,FalseAnswer3" +
+                " from "+ TABLE_NAME+" WHERE Difficulty=\""+diff+"\"",null);
+        return res;
+    }
+}
---
+++ app/src/main/java/com/example/dennis/nerdquiz/Start.java
@@ -0,0 +1,331 @@
+package com.example.dennis.nerdquiz;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class Start extends AppCompatActivity {
+    SharedPreferences shared_preferences;
+    DatabaseHelper myDB;
+    TextView question;
+    Button btn1, btn2, btn3, btn4;
+    ArrayList QuestionAndButtons;
+    String[] QuestionAndButtonsParts;
+    String firstQuestion = "";
+    String whichQuiz = "";
+    int rightAnswer = 0;
+    Random rand = new Random();
+    Random r = new Random();
+
+
+    private ProgressDialog loading;
+    private static final String JSON_ARRAY = "server_response";
+    private JSONArray ques = null;
+
+    int QuestionNumber = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        shared_preferences = getSharedPreferences("shared_preferences_test", MODE_PRIVATE);
+        whichQuiz = shared_preferences.getString("Number", "Default");
+        rightAnswer = r.nextInt(4);
+        if(whichQuiz == "1") {
+            getData("", "", String.valueOf(QuestionNumber));
+        }else{
+            String Diff,Cate;
+            Diff = shared_preferences.getString("Difficulty", "Default");
+            Cate = shared_preferences.getString("Category", "Default");
+            getData(Diff, Cate, String.valueOf(QuestionNumber));
+        }
+
+    }
+
+    private ArrayList extractJSON(String response) {
+        ArrayList<String> list = new ArrayList<String>();
+        StringBuffer buffer = new StringBuffer();
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            ques = jsonObject.getJSONArray(JSON_ARRAY);        //Hier Parsen hopefully
+            for (int i = 0; i < ques.length(); i++) {
+                JSONObject jsonobject = ques.getJSONObject(i);
+                String Question = jsonobject.getString("Question");
+                String RA = jsonobject.getString("RA");
+                String FA1 = jsonobject.getString("FA1");
+                String FA2 = jsonobject.getString("FA2");
+                String FA3 = jsonobject.getString("FA3");
+                buffer.append(Question + ";");
+                buffer.append(RA + ";");
+                buffer.append(FA1 + ";");
+                buffer.append(FA2 + ";");
+                buffer.append(FA3 + ";");
+                list.add(buffer.toString());
+                buffer.delete(0, buffer.length());
+            }
+
+        } catch (JSONException e) {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public String Question(ArrayList questionList) {
+        String question;
+        int i = rand.nextInt(questionList.size());
+        question = questionList.get(i).toString();
+        questionList.remove(i);
+        return question;
+    }
+
+    public void next() {
+        rightAnswer = r.nextInt(4);
+        firstQuestion = Question(QuestionAndButtons);
+        QuestionAndButtonsParts = firstQuestion.split(";");
+        NameButtons();
+    }
+
+    public void NameButtons() {
+        question = (TextView) findViewById(R.id.question1);
+        question.setText(QuestionAndButtonsParts[0]);
+        btn1 = (Button) findViewById(R.id.button1);
+        btn2 = (Button) findViewById(R.id.button2);
+        btn3 = (Button) findViewById(R.id.button3);
+        btn4 = (Button) findViewById(R.id.button4);
+        //mix all buttons
+        mixButtons(rightAnswer, QuestionAndButtonsParts[1], QuestionAndButtonsParts[2], QuestionAndButtonsParts[3], QuestionAndButtonsParts[4]);
+
+    }
+
+
+    public void mixButtons(int i, final String RA, String FA1, String FA2, String FA3) {
+
+
+            switch (i) {
+                case 0:
+                    btn1.setText(RA);
+                    btn2.setText(FA1);
+                    btn3.setText(FA2);
+                    btn4.setText(FA3);
+                    break;
+                case 1:
+                    btn1.setText(FA1);
+                    btn2.setText(RA);
+                    btn3.setText(FA2);
+                    btn4.setText(FA3);
+                    break;
+                case 2:
+                    btn1.setText(FA2);
+                    btn2.setText(FA1);
+                    btn3.setText(RA);
+                    btn4.setText(FA3);
+                    break;
+                case 3:
+                    btn1.setText(FA3);
+                    btn2.setText(FA1);
+                    btn3.setText(FA2);
+                    btn4.setText(RA);
+                    break;
+                default:
+                    break;
+            }
+            btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (btn1.getText() == RA) {
+                        Toast.makeText(getApplicationContext(), "10Punkte", Toast.LENGTH_SHORT)
+                                .show();
+                        if (QuestionAndButtons.size() > 0) {
+                            next();
+                        } else {
+                            finish();
+                            startActivity(new Intent(Start.this, MainActivity.class));
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Falsch", Toast.LENGTH_SHORT)
+                                .show();
+                        if (QuestionAndButtons.size() > 0) {
+                            next();
+                        } else {
+                            startActivity(new Intent(Start.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                }
+            });
+            btn2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (btn2.getText() == RA) {
+                        Toast.makeText(getApplicationContext(), "Korrekt", Toast.LENGTH_SHORT)
+                                .show();
+                        if (QuestionAndButtons.size() > 0) {
+                            next();
+                        } else {
+                            finish();
+                            startActivity(new Intent(Start.this, MainActivity.class));
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Falsch", Toast.LENGTH_SHORT)
+                                .show();
+                        if (QuestionAndButtons.size() > 0) {
+                            next();
+                        } else {
+                            startActivity(new Intent(Start.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                }
+            });
+            btn3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (btn3.getText() == RA) {
+                        Toast.makeText(getApplicationContext(), "Korrekt", Toast.LENGTH_SHORT)
+                                .show();
+                        if (QuestionAndButtons.size() > 0) {
+                            next();
+                        } else {
+                            finish();
+                            startActivity(new Intent(Start.this, MainActivity.class));
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Falsch", Toast.LENGTH_SHORT)
+                                .show();
+                        if (QuestionAndButtons.size() > 0) {
+                            next();
+                        } else {
+                            startActivity(new Intent(Start.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                }
+            });
+            btn4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (btn4.getText() == RA) {
+                        Toast.makeText(getApplicationContext(), "Korrekt", Toast.LENGTH_SHORT)
+                                .show();
+                        if (QuestionAndButtons.size() > 0) {
+                            next();
+                        } else {
+                            finish();
+                            startActivity(new Intent(Start.this, MainActivity.class));
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Falsch", Toast.LENGTH_SHORT)
+                                .show();
+                        if (QuestionAndButtons.size() > 0) {
+                            next();
+                        } else {
+                            startActivity(new Intent(Start.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                }
+            });
+
+    }
+
+    private void getData(String Diff,String Cate,String Num){
+        final String Difficulty,Category,Number;
+        Difficulty = Diff;
+        Category = Cate;
+        Number = Num;
+     class GetDataJSON extends AsyncTask<String, Void, String> {
+        private ProgressDialog progressDialog;
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(Start.this, "Downloading Quiz", "Wait....", true);
+        }
+
+        protected String doInBackground(String... params) {
+            String JSON_STRING;
+            String result = "";
+            try {
+                String json_url = "http://quizdb.net23.net/json_get_data.php";
+                URL url = new URL(json_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream OS = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+                String data =
+                        URLEncoder.encode("Category", "UTF-8") + "=" + URLEncoder.encode(Category, "UTF-8") + "&" +
+                        URLEncoder.encode("Difficulty", "UTF-8") + "=" + URLEncoder.encode(Difficulty, "UTF-8") + "&" +
+                URLEncoder.encode("Number", "UTF-8") + "=" + URLEncoder.encode(Number, "UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((JSON_STRING = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(JSON_STRING + "\n");
+                }
+
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                result = stringBuilder.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            QuestionAndButtons = extractJSON(result);
+            firstQuestion = Question(QuestionAndButtons);
+            QuestionAndButtonsParts = firstQuestion.split(";");
+
+            NameButtons();
+        }
+    }
+        GetDataJSON asyncObject = new GetDataJSON();
+        asyncObject.execute("");
+}
+}
