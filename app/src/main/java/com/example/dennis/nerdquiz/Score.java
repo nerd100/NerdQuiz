2c6414acbabe168f0aaa431b82ef0b4eb2426ecb
KrokoDog <luigi.grimaldi@hotmail.de>
2016-09-09 21:48:14 +0200

    cleaned start activity, added score and globalstat, images added

---
+++ app/src/main/java/com/example/dennis/nerdquiz/Score.java
@@ -0,0 +1,18 @@
+package com.example.dennis.nerdquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by SirLui on 09.09.2016.
+ */
+public class Score extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+    }
+
+}
--- app/src/main/java/com/example/dennis/nerdquiz/Statistic.java
+++
@@ -1,8 +0,0 @@
-package com.example.dennis.nerdquiz;
-
-/**
- * Created by SirLui on 09.09.2016.
- */
-public class Statistic {
-    //Get all statistics
-}
